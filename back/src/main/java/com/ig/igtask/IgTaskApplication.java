27e0e00de60dc8334e9e1a8ba9dd5dea33c20aa3
SlintPunk <artur.matuszkiewicz@gmail.com>
2020-10-10 18:57:59 +0200

    Update front

--- back/src/main/java/com/ig/igtask/IgTaskApplication.java
+++ back/src/main/java/com/ig/igtask/IgTaskApplication.java
@@ -9,7 +9,6 @@ public class IgTaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IgTaskApplication.class, args);
-
 	}
 
 }
--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppWrapper from "carbon-react/lib/components/app-wrapper";
+import { Table, TableRow, TableCell, TableHeader } from "carbon-react/lib/components/table";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <Table>
+        <TableRow>Row row</TableRow>
+      </Table>
+    </AppWrapper>
   );
+
 }
 
 export default App;
